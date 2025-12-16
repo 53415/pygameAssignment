30bcb2d67f889f9004f41954dbaf90e5a9efbf36
1 <1qadribil@hdsb.ca>
2025-12-16 11:07:55 -0500

    added menu

--- assignment.py
+++ assignment.py
@@ -50,7 +50,7 @@ import math
 import os
 import time
 import random
-
+import gif_pygame
 pygame.init()
 
 # *********SETUP**********
@@ -62,7 +62,15 @@ clock = pygame.time.Clock()  #will allow us to set framerate
 bahrainTrack = pygame.transform.scale(pygame.image.load("bahrain.png"), (12800,7200))
 bahrainMinimap = pygame.transform.scale(pygame.image.load("BahrainMinimap.png"), (256,144))
 
+menu = pygame.transform.scale(pygame.image.load("menu.png"), (1280,720))
+
 car = pygame.image.load("ferrari.png")
+
+f1logo = pygame.transform.scale(pygame.image.load("F1logo.png"), (388.5,100))
+f1font = pygame.font.Font("f1font.ttf", 60)
+f1font2 = pygame.font.Font("f1font.ttf", 30)
+
+fireworks = gif_pygame.load("giphy.gif")
 
 ferrari = pygame.image.load("ferrari.png")
 Mclaren = pygame.image.load("Mclaren.png")
@@ -125,7 +133,7 @@ RRTWC = tyresG
 
 i = 0
 
-lightSound.play()
+gamestate = "main"
 
 # *********GAME LOOP**********
 while True:
@@ -137,6 +145,13 @@ while True:
     ev = pygame.event.poll()    # Look for any event
     if ev.type == pygame.QUIT:  # windowow close button clicked?
         break                   #   ... leave game loop
+    if ev.type == pygame.MOUSEBUTTONDOWN and ev.button == 1:
+            mouseX, mouseY = pygame.mouse.get_pos()
+            if gamestate == "main":
+                if 300 < mouseX < 940 and 270 < mouseY < 370:
+                    gamestate = "start"
+                    lightSound.play()
+
     if ev.type == pygame.KEYDOWN:  
         if ev.key == pygame.K_SPACE:  
             if DRS == False:
@@ -166,201 +181,222 @@ while True:
                
     #PUT YOUR MOUSE/KEYBOARD EVENTS HERE
     
-    # *********GAME LOGIC**********
-    screen.blit(bahrainTrack, (trackX,trackY))
-    
-
-    keys = pygame.key.get_pressed()
-
-    if lightsOut:
-        if keys[pygame.K_a]:
-            angle+=3
-            FLTW -= 0.01
-        if keys[pygame.K_d]:
-            angle-=3
-            FRTW -= 0.01
-    
-    # Reference #1
-    angleRadians = math.radians(angle)
-    xTravelled = round(speed * math.cos(angleRadians) ,0)
-    yTravelled = round(speed * -math.sin(angleRadians) ,0)
-    # End Reference #1
-    
-    trackX += xTravelled
-    trackY += yTravelled
-
-    if keys[pygame.K_1]:
-        car =   Mclaren
-        maxSpeed = 15
-    elif keys[pygame.K_2]:
-        car = Mercedes
-        maxSpeed = 14.5
-    elif keys[pygame.K_3]:
-        car = Redbull
-        maxSpeed = 14
-    elif keys[pygame.K_4]:
-        car = VCARB
-        maxSpeed = 13.5
-    elif keys[pygame.K_5]:
-        car = ferrari
-        maxSpeed = 13
-    elif keys[pygame.K_6]:
-        car = Williams
-        maxSpeed = 12.5
-    elif keys[pygame.K_7]:
-        car = AstonMartin
-        maxSpeed = 12
-    elif keys[pygame.K_8]:
-        car = Haas
-        maxSpeed = 11.5
-    elif keys[pygame.K_9]:
-        car = Sauber
-        maxSpeed = 11
-    elif keys[pygame.K_0]:
-        car = Alpine
-        maxSpeed = 10.5
-
-    if keys[pygame.K_s]:
-        if speed > 0:
-            speed -= 0.1
-
-    if keys[pygame.K_w] and lightsOut:
-        if speed < maxSpeed:
-            speed += 0.1
-        if RRTW >= 0 and RLTW >= 0:
-            RLTW -= 0.01
-            RRTW -= 0.01
-    else:
-        if speed > 0:
-            speed -= 0.1
-    
-
-    if DRS:
-        if keys[pygame.K_w] and speed < maxSpeed +5:
-            speed += 0.2
-    else:
-        if speed > maxSpeed:
-            speed -= 0.1
-
-    if (round(trackX,0)) < -6600.0 and (round(trackX,0)) > -6650.0 and trackY > -6347 and trackY < -6034 and not crossing:
-        lap += 1
-        crossing = True
-
-
-    
-    if lap == 1:
-        lap1time = round( time,2)
-    elif lap == 2:
-        lap2time = round(time - lap1time,2)
-
-    elif lap == 3:
-        lap3time =  round(time - lap2time - lap1time,2)
-    if not ((round(trackX,0)) < -6600.0 and (round(trackX,0)) > -6650.0 and trackY > -6347 and trackY < -6034):
-        crossing = False
-
-
-    screen.blit(font.render(("lap # " + str(lap)), True, (255, 255, 255)) , (20, 20)) 
-    screen.blit(font.render((str(int(20*speed))) + " km/h", True, (255, 255, 255)) , (600, 20)) 
-    
-    screen.blit(font.render(("lap 1: " +str(lap1time)), True, ("white")) , (1050, 20))
-    if lap2time > 0:
-        screen.blit(font.render(("lap 2: " +str(lap2time)), True, ("white")) , (1050, 50))
-    if lap3time > 0:
-        screen.blit(font.render(("lap 3: " +str(lap3time)), True, ("white")) , (1050, 80)) 
-
-    
-    if lap == 2:
-        if lap1time < lap2time:
-            screen.blit(font.render(("lap 1: " +str(lap1time)), True, (85, 26, 139)) , (1050, 20))
-        else:
-            screen.blit(font.render(("lap 2: " +str(lap2time)), True, (85, 26, 139)) , (1050, 50))
-
-    if lap >= 3 :
-        if lap1time < lap2time and lap1time < lap3time:
-            screen.blit(font.render(("lap 1: " +str(lap1time)), True, (85, 26, 139)) , (1050, 20))
-        else:
-            if lap2time < lap3time:
-                screen.blit(font.render(("lap 2: " +str(lap2time)), True, (85, 26, 139)) , (1050, 50))
-            else:
-                screen.blit(font.render(("lap 3: " +str(lap3time)), True, (85, 26, 139)) , (1050, 80))
-    
-    
-    if FLTW > 80:
-        FLTWC = tyresG
-    elif FLTW > 50:
-        FLTWC = tyresY
-    else:
-        FLTWC = tyresR
-    if FRTW > 80:
-        FRTWC = tyresG
-    elif FRTW > 50:
-        FRTWC = tyresY
-    else:
-        FRTWC = tyresR
-    if RLTW > 80:
-        RLTWC = tyresG
-    elif RLTW > 50:
-        RLTWC = tyresY
-    else:
-        RLTWC = tyresR
-    if RRTW > 80:
-        RRTWC = tyresG
-    elif RRTW > 50:
-        RRTWC = tyresY
-    else:
-        RRTWC = tyresR
-    
-    screen.blit((FLTWC), (1118, 510))
-    screen.blit((FRTWC), (1200, 510))
-    screen.blit((RLTWC), (1118, 610))
-    screen.blit((RRTWC), (1200, 610))
-
-    screen.blit(font.render(str(int(FLTW)), True, (255, 255, 255)) , (1130, 530))
-    screen.blit(font.render(str(int(FRTW)), True, (255, 255, 255)) , (1215, 530))
-    screen.blit(font.render(str(int(RLTW)), True, (255, 255, 255)) , (1130, 640))
-    screen.blit(font.render(str(int(RRTW)), True, (255, 255, 255)) , (1215, 640))
-
-    screen.blit((bahrainMinimap), (20, 550))
-
-    
-
-    if DRS:
-        screen.blit(pygame.transform.rotate(carDRS, angle), (500, 300))
-    else:
-        screen.blit(pygame.transform.rotate(car, angle), (500, 300))
-    if speed > 0:
-        if i < 1:
-            screen.blit(pygame.transform.rotate(treads1, angle), (500, 300))
-            i += 0.1
-        elif i < 2:
-            screen.blit(pygame.transform.rotate(treads2, angle), (500, 300))
-            i += 0.1
-        else:
-            i = 0  
-
-    if -1 > lights <= 0:
-        screen.blit(lights0, (380, 110))
-    elif 0 > lights <= 1:
-        screen.blit(lights1, (380, 110))
-    elif 1 > lights <= 2:
-        screen.blit(lights2, (380, 110))
-    elif 2 > lights <= 3:
-        screen.blit(lights3, (380, 110))
-    elif 3 > lights <= 4:
-        screen.blit(lights4, (380, 110))
-    elif round(lights,0) >= 3:
-        lightsOut = True
+    if gamestate == "start":
+        # *********GAME LOGIC**********
+        screen.blit(bahrainTrack, (trackX,trackY))
         
-    if lights <= 3:
-        lights += random.uniform(0.01, 0.02)
-    else:
-        lightsOut == True
+
+        keys = pygame.key.get_pressed()
+
+        if lightsOut:
+            if keys[pygame.K_a]:
+                angle+=3
+                FLTW -= 0.01
+            if keys[pygame.K_d]:
+                angle-=3
+                FRTW -= 0.01
+        
+        # Reference #1
+        angleRadians = math.radians(angle)
+        xTravelled = round(speed * math.cos(angleRadians) ,0)
+        yTravelled = round(speed * -math.sin(angleRadians) ,0)
+        # End Reference #1
+        
+        trackX += xTravelled
+        trackY += yTravelled
+
+        if keys[pygame.K_1]:
+            car =   Mclaren
+            maxSpeed = 15
+        elif keys[pygame.K_2]:
+            car = Mercedes
+            maxSpeed = 14.5
+        elif keys[pygame.K_3]:
+            car = Redbull
+            maxSpeed = 14
+        elif keys[pygame.K_4]:
+            car = VCARB
+            maxSpeed = 13.5
+        elif keys[pygame.K_5]:
+            car = ferrari
+            maxSpeed = 13
+        elif keys[pygame.K_6]:
+            car = Williams
+            maxSpeed = 12.5
+        elif keys[pygame.K_7]:
+            car = AstonMartin
+            maxSpeed = 12
+        elif keys[pygame.K_8]:
+            car = Haas
+            maxSpeed = 11.5
+        elif keys[pygame.K_9]:
+            car = Sauber
+            maxSpeed = 11
+        elif keys[pygame.K_0]:
+            car = Alpine
+            maxSpeed = 10.5
+
+        if keys[pygame.K_s]:
+            if speed > 0:
+                speed -= 0.1
+
+        if keys[pygame.K_w] and lightsOut:
+            if speed < maxSpeed:
+                speed += 0.1
+            if RRTW >= 0 and RLTW >= 0:
+                RLTW -= 0.01
+                RRTW -= 0.01
+        else:
+            if speed > 0:
+                speed -= 0.1
+        
+
+        if DRS:
+            if keys[pygame.K_w] and speed < maxSpeed +5:
+                speed += 0.2
+        else:
+            if speed > maxSpeed:
+                speed -= 0.1
+
+        if (round(trackX,0)) < -6600.0 and (round(trackX,0)) > -6650.0 and trackY > -6347 and trackY < -6034 and not crossing:
+            lap += 1
+            crossing = True
 
 
-    minimapX = trackX / -50 + 25
-    minimapY = trackY / -50 + 560
-    pygame.draw.circle(screen, (255,0,0),(minimapX,minimapY ), 7)
-    speed = round(speed,1)
+        
+        if lap == 1:
+            lap1time = round( time,2)
+        elif lap == 2:
+            lap2time = round(time - lap1time,2)
+
+        elif lap == 3:
+            lap3time =  round(time - lap2time - lap1time,2)
+        if not ((round(trackX,0)) < -6600.0 and (round(trackX,0)) > -6650.0 and trackY > -6347 and trackY < -6034):
+            crossing = False
+
+
+        screen.blit(f1font2.render(("lap # " + str(lap)), True, (255, 255, 255)) , (20, 20)) 
+        screen.blit(f1font2.render((str(int(20*speed))) + " km/h", True, (255, 255, 255)) , (600, 20)) 
+        
+        screen.blit(f1font2.render(("lap 1: " +str(lap1time)), True, ("white")) , (1050, 20))
+        if lap2time > 0:
+            screen.blit(f1font2.render(("lap 2: " +str(lap2time)), True, ("white")) , (1050, 50))
+        if lap3time > 0:
+            screen.blit(f1font2.render(("lap 3: " +str(lap3time)), True, ("white")) , (1050, 80)) 
+
+        
+        if lap == 2:
+            if lap1time < lap2time:
+                screen.blit(f1font2.render(("lap 1: " +str(lap1time)), True, (85, 26, 139)) , (1050, 20))
+            else:
+                screen.blit(f1font2.render(("lap 2: " +str(lap2time)), True, (85, 26, 139)) , (1050, 50))
+
+        if lap >= 3 :
+            if lap1time < lap2time and lap1time < lap3time:
+                screen.blit(f1font2.render(("lap 1: " +str(lap1time)), True, (85, 26, 139)) , (1050, 20))
+            else:
+                if lap2time < lap3time:
+                    screen.blit(f1font2.render(("lap 2: " +str(lap2time)), True, (85, 26, 139)) , (1050, 50))
+                else:
+                    screen.blit(f1font2.render(("lap 3: " +str(lap3time)), True, (85, 26, 139)) , (1050, 80))
+        
+        
+        if FLTW > 80:
+            FLTWC = tyresG
+        elif FLTW > 50:
+            FLTWC = tyresY
+        else:
+            FLTWC = tyresR
+        if FRTW > 80:
+            FRTWC = tyresG
+        elif FRTW > 50:
+            FRTWC = tyresY
+        else:
+            FRTWC = tyresR
+        if RLTW > 80:
+            RLTWC = tyresG
+        elif RLTW > 50:
+            RLTWC = tyresY
+        else:
+            RLTWC = tyresR
+        if RRTW > 80:
+            RRTWC = tyresG
+        elif RRTW > 50:
+            RRTWC = tyresY
+        else:
+            RRTWC = tyresR
+        
+        screen.blit((FLTWC), (1118, 510))
+        screen.blit((FRTWC), (1200, 510))
+        screen.blit((RLTWC), (1118, 610))
+        screen.blit((RRTWC), (1200, 610))
+
+        screen.blit(f1font2.render(str(int(FLTW)), True, (255, 255, 255)) , (1125, 535))
+        screen.blit(f1font2.render(str(int(FRTW)), True, (255, 255, 255)) , (1208, 535))
+        screen.blit(f1font2.render(str(int(RLTW)), True, (255, 255, 255)) , (1125, 640))
+        screen.blit(f1font2.render(str(int(RRTW)), True, (255, 255, 255)) , (1208, 640))
+
+        screen.blit((bahrainMinimap), (20, 550))
+
+        
+
+        if DRS:
+            screen.blit(pygame.transform.rotate(carDRS, angle), (500, 300))
+        else:
+            screen.blit(pygame.transform.rotate(car, angle), (500, 300))
+        if speed > 0:
+            if i < 1:
+                screen.blit(pygame.transform.rotate(treads1, angle), (500, 300))
+                i += 0.1
+            elif i < 2:
+                screen.blit(pygame.transform.rotate(treads2, angle), (500, 300))
+                i += 0.1
+            else:
+                i = 0  
+
+        if -1 > lights <= 0:
+            screen.blit(lights0, (380, 110))
+        elif 0 > lights <= 1:
+            screen.blit(lights1, (380, 110))
+        elif 1 > lights <= 2:
+            screen.blit(lights2, (380, 110))
+        elif 2 > lights <= 3:
+            screen.blit(lights3, (380, 110))
+        elif 3 > lights <= 4:
+            screen.blit(lights4, (380, 110))
+        elif round(lights,0) >= 3:
+            lightsOut = True
+            
+        if lights <= 3:
+            lights += random.uniform(0.01, 0.02)
+        else:
+            lightsOut == True
+
+
+        minimapX = trackX / -50 + 25
+        minimapY = trackY / -50 + 560
+        pygame.draw.circle(screen, (255,0,0),(minimapX,minimapY ), 7)
+        speed = round(speed,1)
+
+    mouseX, mouseY = pygame.mouse.get_pos()
+
+    if gamestate == "main":
+        screen.blit(menu, (0,0))
+        fireworks.render(screen,(0,0))
+        screen.blit(f1logo, (440,100))
+        if 300 < mouseX < 940 and 430 < mouseY < 530:
+            pygame.draw.rect(screen, ("white"),  (300, 430, 640, 100),0,20)
+        else:
+            pygame.draw.rect(screen, (220, 220, 220), (300, 430, 640, 100),0,20)
+
+        if 300 < mouseX < 940 and 270 < mouseY < 370:
+            pygame.draw.rect(screen, ("white"), (300, 270, 640, 100),0,20)
+        else:
+            pygame.draw.rect(screen, (220, 220, 220), (300, 270, 640, 100),0,20)
+
+        screen.blit(f1font.render('Settings', True, ("black")) , (440, 450)) 
+        screen.blit(f1font.render('Race', True, ("black")) , (520, 290))
+        
     pygame.display.flip()
     clock.tick(60)
 
